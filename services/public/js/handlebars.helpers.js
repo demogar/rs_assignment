c1af12eb4a8f64a3f951b98001a6932dfc90f013
Demostenes Garcia <me@demogar.com>
2013-06-07 16:05:58 -0500

    changed minor issue on select box

---
+++ services/public/js/handlebars.helpers.js
@@ -0,0 +1,7 @@
+(function() {
+	Handlebars.registerHelper('if_eq', function(context, options) {
+		if (context == options.hash.compare)
+			return options.fn(this);
+		return options.inverse(this);
+	});
+})();
